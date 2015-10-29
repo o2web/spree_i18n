d8ef03e46bb3deb445b898a51d909810de67c1b1
PatLeb <PatLeb@users.noreply.github.com>
2015-10-29 10:50:57 -0400

    remove routing-filter with locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.add_routes do
   # from routing-filter gem
-  filter :locale
+  # filter :locale
 
   post '/locale/set', to: 'locale#set', defaults: { format: :json }, as: :set_locale
 end
